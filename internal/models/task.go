b334cdb82edcfcf9743f83c421cc5e7233cf7d08
sunny5156 <sunny5156@qq.com>
2019-03-16 16:34:45 +0800

    修改任务标题长度

--- internal/models/task.go
+++ internal/models/task.go
@@ -39,13 +39,13 @@ const (
 // 任务
 type Task struct {
 	Id               int                  `json:"id" xorm:"int pk autoincr"`
-	Name             string               `json:"name" xorm:"varchar(32) notnull"`                            // 任务名称
+	Name             string               `json:"name" xorm:"varchar(250) notnull"`                            // 任务名称
 	Level            TaskLevel            `json:"level" xorm:"tinyint notnull index default 1"`               // 任务等级 1: 主任务 2: 依赖任务
 	DependencyTaskId string               `json:"dependency_task_id" xorm:"varchar(64) notnull default ''"`   // 依赖任务ID,多个ID逗号分隔
 	DependencyStatus TaskDependencyStatus `json:"dependency_status" xorm:"tinyint notnull default 1"`         // 依赖关系 1:强依赖 主任务执行成功, 依赖任务才会被执行 2:弱依赖
 	Spec             string               `json:"spec" xorm:"varchar(64) notnull"`                            // crontab
 	Protocol         TaskProtocol         `json:"protocol" xorm:"tinyint notnull index"`                      // 协议 1:http 2:系统命令
-	Command          string               `json:"command" xorm:"varchar(256) notnull"`                        // URL地址或shell命令
+	Command          string               `json:"command" xorm:"varchar(500) notnull"`                        // URL地址或shell命令
 	HttpMethod       TaskHTTPMethod       `json:"http_method" xorm:"tinyint notnull default 1"`               // http请求方法
 	Timeout          int                  `json:"timeout" xorm:"mediumint notnull default 0"`                 // 任务执行超时时间(单位秒),0不限制
 	Multi            int8                 `json:"multi" xorm:"tinyint notnull default 1"`                     // 是否允许多实例运行
@@ -55,7 +55,7 @@ type Task struct {
 	NotifyType       int8                 `json:"notify_type" xorm:"tinyint notnull default 0"`               // 通知类型 1: 邮件 2: slack 3: webhook
 	NotifyReceiverId string               `json:"notify_receiver_id" xorm:"varchar(256) notnull default '' "` // 通知接受者ID, setting表主键ID，多个ID逗号分隔
 	NotifyKeyword    string               `json:"notify_keyword" xorm:"varchar(128) notnull default '' "`
-	Tag              string               `json:"tag" xorm:"varchar(32) notnull default ''"`
+	Tag              string               `json:"tag" xorm:"varchar(50) notnull default ''"`
 	Remark           string               `json:"remark" xorm:"varchar(100) notnull default ''"` // 备注
 	Status           Status               `json:"status" xorm:"tinyint notnull index default 0"` // 状态 1:正常 0:停止
 	Created          time.Time            `json:"created" xorm:"datetime notnull created"`       // 创建时间
